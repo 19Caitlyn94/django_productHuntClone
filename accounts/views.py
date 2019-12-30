ecc68b7ce3e25ab00cf8e56b0149b681bfef4115
CaitlynFowkes <caitlyn@gradconnection.com>
2019-12-30 13:20:15 +0200

    Logout setup

--- accounts/views.py
+++ accounts/views.py
@@ -20,9 +20,17 @@ def signup(request):
         return render(request, 'accounts/signup.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else: return render(request, 'accounts/login.html', {'error': 'Invalid login credentials'})
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
-    # TODO Need to route to homepage
-    # Dont forget to logout
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
     return render(request, 'accounts/signup.html')
